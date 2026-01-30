9c4684f8354069d2766ba009879661d01c97717b
louisk123 <louiskaram@gmail.com>
2026-01-30 11:38:18 +0400

    Update bor_main.py

--- bor_main.py
+++ bor_main.py
@@ -307,6 +307,7 @@ def get_first_line(file_path,cinema_map):  #chekc if file is pdf or excel and tr
                 if text == "Ticket Types Per Title" and len(lines) > 3:
                     text = lines[3].replace("Selection", "").strip()
                 
+                st.write("ntital tex",text)
                 if text.upper() == "AL MARIAH MALL ABU DHABHI":
                     text = text.upper() + " " + lines.split("\n")[1].strip().upper()
                     st.write(text)
