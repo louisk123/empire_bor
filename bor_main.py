280bc8ebd9a1055e458f22794fb3ae5ebecef53e
louisk123 <louiskaram@gmail.com>
2026-01-30 11:35:44 +0400

    Update bor_main.py

--- bor_main.py
+++ bor_main.py
@@ -309,6 +309,7 @@ def get_first_line(file_path,cinema_map):  #chekc if file is pdf or excel and tr
                 
                 if text.upper() == "AL MARIAH MALL ABU DHABHI":
                     text = text.upper() + " " + lines.split("\n")[1].strip().upper()
+                    st.write(text)
     
                 return text
     
